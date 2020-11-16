855cea041b1c404233cd0df8c8bba7a54e6ef9bc
김지웅 <wldnd9100@naver.com>
2020-11-16 22:36:24 +0900

    client_src

---
+++ WF_CloudClient/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace WF_CloudClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.File = new System.Windows.Forms.Button();
+            this.File_Address = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.File_Upload = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btn_download = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "\"\"";
+            this.openFileDialog1.InitialDirectory = "\"\"";
+            // 
+            // File
+            // 
+            this.File.Location = new System.Drawing.Point(260, 352);
+            this.File.Name = "File";
+            this.File.Size = new System.Drawing.Size(100, 26);
+            this.File.TabIndex = 1;
+            this.File.Text = "파일 선택";
+            this.File.UseVisualStyleBackColor = true;
+            this.File.Click += new System.EventHandler(this.File_Select);
+            // 
+            // File_Address
+            // 
+            this.File_Address.Location = new System.Drawing.Point(22, 352);
+            this.File_Address.Multiline = true;
+            this.File_Address.Name = "File_Address";
+            this.File_Address.ReadOnly = true;
+            this.File_Address.Size = new System.Drawing.Size(200, 24);
+            this.File_Address.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(22, 49);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(197, 259);
+            this.listBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(82, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "파일 목록";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(260, 235);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 26);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "삭제";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.File_delete);
+            // 
+            // File_Upload
+            // 
+            this.File_Upload.Location = new System.Drawing.Point(260, 402);
+            this.File_Upload.Name = "File_Upload";
+            this.File_Upload.Size = new System.Drawing.Size(100, 26);
+            this.File_Upload.TabIndex = 8;
+            this.File_Upload.Text = "파일 업로드";
+            this.File_Upload.UseVisualStyleBackColor = true;
+            this.File_Upload.Click += new System.EventHandler(this.File_Upload_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(260, 168);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 26);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "새로고침";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.File_load_Click);
+            // 
+            // btn_download
+            // 
+            this.btn_download.Location = new System.Drawing.Point(260, 104);
+            this.btn_download.Name = "btn_download";
+            this.btn_download.Size = new System.Drawing.Size(100, 26);
+            this.btn_download.TabIndex = 10;
+            this.btn_download.Text = "다운로드";
+            this.btn_download.UseVisualStyleBackColor = true;
+            this.btn_download.Click += new System.EventHandler(this.btn_download_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 450);
+            this.Controls.Add(this.btn_download);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.File_Upload);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.File_Address);
+            this.Controls.Add(this.File);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "클라우드";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button File;
+        private System.Windows.Forms.TextBox File_Address;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button File_Upload;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btn_download;
+    }
+}
+
---
+++ WF_CloudClient/Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using NPOI.Util;
+
+namespace WF_CloudClient
+{
+    public partial class Form1 : Form
+    {
+        // 업로드 파일 경로
+        String file_path = null;
+        // 서버에 접속한다.
+        IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("192.168.137.38"), 3333);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // 업로드할 파일 선택
+        private void File_Select(object sender, EventArgs e)
+        {
+            File_Address.Clear();
+            openFileDialog1.InitialDirectory = "C:\\";
+            //openFileDialog의 시작 위치를 C:\\로 설정
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                file_path = openFileDialog1.FileName;
+                // 선택된 파일의 풀 경로를 불러와 저장
+                File_Address.Text = file_path.Split('\\')[file_path.Split('\\').Length - 1];
+                // 해당 경로에서 파일명만 추출하여 textBox에 표시
+            }
+        }
+
+
+        // 업로드 버튼으로 업로드
+        private void File_Upload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+
+                // FileInfo 생성
+                FileInfo file = new FileInfo(file_path);
+                // 파일이 존재하는지
+                if (file.Exists)
+                {
+                    // 바이너리 버퍼
+                    var binary = new byte[file.Length];
+                    // 파일 IO 생성
+                    using (var stream = new FileStream(file.FullName, FileMode.Open, FileAccess.Read))
+                    {
+                        // 소켓 생성
+                        using (Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+                        {
+                            // 접속
+                            client.Connect(ipep);
+
+                            // 파일을 IO로 읽어온다.
+                            stream.Read(binary, 0, binary.Length);
+
+                            byte[] bytes = new byte[2];
+
+                            client.Send(Encoding.UTF8.GetBytes("upload"));
+
+
+                            client.Receive(bytes);
+                            client.Send(Encoding.UTF8.GetBytes(binary.Length.ToString()));
+
+                            client.Receive(bytes);
+                            client.Send(Encoding.UTF8.GetBytes(file.Name));
+
+                            client.Receive(bytes);
+                            client.Send(binary);
+
+
+
+                        }
+                    }
+                    File_load();
+                }
+                else
+                {
+                    MessageBox.Show("파일이 존재하지 않습니다.");
+                    //Console.WriteLine("파일이 존재하지 않습니다.");
+                }
+            }
+            catch (ArgumentNullException)
+            {
+                //MessageBox.Show("파일을 선택해 주세요.");
+                //Console.WriteLine("파일을 선택해 주세요.");
+            }
+
+        }
+
+
+        // 파일 로드
+        private void File_load_Click(object sender, EventArgs e)
+        {
+            File_load();
+        }
+
+        private void File_load()
+        {
+            try
+            {
+                // 소켓 생성
+                using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    // 접속
+                    socket.Connect(ipep);
+                    byte[] data = new byte[100];
+                    // 파일을 IO로 읽어온다.
+                    //stream.Read(binary, 0, binary.Length);
+                    socket.Send(Encoding.UTF8.GetBytes("list"));
+
+                    socket.Receive(data);
+
+                    //리스트 초기화
+                    listBox1.Items.Clear();
+
+                    int list_size = BitConverter.ToInt32(data, 0) - 48;
+
+                    socket.Send(Encoding.UTF8.GetBytes("ok"));
+
+                    Console.WriteLine("Recieved file_count: " + list_size);
+
+                    for (int j = 0; j < list_size; j++)
+                    {
+                        int size = socket.Receive(data);
+                        string title = string.Empty;
+                        for (int i = 0; i < size; i++)
+                            title += Convert.ToChar(data[i]);
+                        socket.Send(Encoding.UTF8.GetBytes("ok"));
+                        listBox1.Items.Add(title);
+                        Console.WriteLine("Recieved data: " + title + "\n" + title.Length);
+                    }
+
+
+                }
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("파일을 선택해 주세요.");
+                //Console.WriteLine("파일을 선택해 주세요.");
+            }
+        }
+
+        // 파일 삭제
+        private void File_delete(object sender, EventArgs e)
+        {
+            try
+            {
+
+                // 소켓 생성
+                using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    // 접속
+                    socket.Connect(ipep);
+
+                    socket.Send(Encoding.UTF8.GetBytes("delete"));
+                    string file_name = "file";
+                    byte[] data = new byte[100];
+                    socket.Receive(data);
+                    if (listBox1.SelectedItem != null)
+                    {
+                        file_name = listBox1.SelectedItem.ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("리스트에서 파일을 선택해 주세요.");
+                    }
+
+                    socket.Send(Encoding.UTF8.GetBytes(file_name));
+
+                    // 다시 한번 로드
+                    File_load();
+                }
+
+            }
+            catch (ArgumentNullException)
+            {
+                MessageBox.Show("파일을 선택해 주세요.");
+                //Console.WriteLine("파일을 선택해 주세요.");
+            }
+        }
+
+        // 파일 다운로드
+        private void btn_download_Click(object sender, EventArgs e)
+        {
+            // 다운받을 파일의 이름
+            string file_name = "file";
+            // 파일 다운로드 경로
+            string file_download_path = "";
+
+            try
+            {
+                // 소켓 생성
+                using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    // 접속
+                    socket.Connect(ipep);
+
+                    socket.Send(Encoding.UTF8.GetBytes("download"));
+                    byte[] size_size = new byte[100];
+
+                    // ok 받기
+                    socket.Receive(size_size);
+
+
+                    if (listBox1.SelectedItem != null)
+                    {
+                        // 파일 이름 가져오기
+                        file_name = listBox1.SelectedItem.ToString();
+                        // 파일 이름 보내기
+                        socket.Send(Encoding.UTF8.GetBytes(file_name));
+
+                        // 파일 사이즈 받기
+                        int size = socket.Receive(size_size);
+                        string contents = string.Empty;
+                        // 숫자로 바꿀때 아스키코드로 와서 48을 빼줌
+                        for (int i = 0; i < size; i++)
+                        {
+                            int a = Convert.ToInt32(size_size[i])-48;
+                            contents += (a.ToString());
+                        }
+
+                        Console.WriteLine("contents : " + contents);
+
+                        //byte[] data = new byte[Int32.Parse(contents)];
+                        byte[] data = new byte[1460];
+                        // ok 보내기
+                        socket.Send(Encoding.UTF8.GetBytes("ok"));
+
+                        byte[] c;
+                        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                        // file 내용 while
+                        while (true)
+                        {
+                            socket.Receive(data);
+
+                            outputStream.Write(data);
+
+                            c = outputStream.ToByteArray();
+
+                            Console.WriteLine("part len : " + data.Length);
+                            
+                            if (c.Length >= Int32.Parse(contents))
+                            {
+                                
+                                Console.WriteLine("filedata len : " + c.Length);
+                                break;
+                            }
+                        }
+
+                        // 파일 경로 열기
+                        FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+                        if (fbd.ShowDialog() == DialogResult.OK)
+                        {
+                            file_download_path = fbd.SelectedPath;
+
+                        }
+
+                        FileStream fs = new FileStream(@file_download_path + "\\" + file_name, FileMode.OpenOrCreate, FileAccess.Write);
+
+                        fs.Write(c, 0, Int32.Parse(contents));
+                        fs.Close();
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("리스트에서 파일을 선택해 주세요.");
+                    }
+                }
+
+            }
+            catch (ArgumentNullException)
+            {
+                MessageBox.Show("파일을 선택해 주세요.");
+            }
+        }
+
+        
+    }
+}
